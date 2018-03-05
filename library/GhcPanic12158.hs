82384f23dedc17ab4548f8b53a91641d04c89204
Phil de Joux <philderbeast@gmail.com>
2018-03-05 11:26:32 -0500

    Simplify function name.

--- library/GhcPanic12158.hs
+++ library/GhcPanic12158.hs
@@ -5,5 +5,5 @@ module GhcPanic12158 where
 import qualified Geodesy as G (X(..))
 import Geodesy (Y(..))
 
-updateDistance :: G.X a -> G.X a
-updateDistance G.X{x} = G.X{x = x}
+update :: G.X a -> G.X a
+update G.X{x} = G.X{x = x}
